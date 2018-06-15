37f0d295ce1529d6ae683c0a723483f15b49b8cf
Kiyotaka Mukasa <kytk.khhut.328@gmail.com>
2018-06-16 07:28:38 +0900

    modified

--- mklink.sh
+++ mklink.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # create symbolic link
 ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
